e7fd698ef17c07c5f22f43162809a3b2e840d49c
Vinod26499 <vk1611.rkt@gmail.com>
2026-02-09 19:21:42 +0000

    Adding s3.tf and plan output

---
+++ s3.tf
@@ -0,0 +1,9 @@
+#copy and paste the code, and provide unique name to bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
